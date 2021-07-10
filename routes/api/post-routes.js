7d07c74a4eca51ecea3bf0b2daefa1d0aa1f8d1f
LAgrl <lizethariasc14@gmail.com>
2021-07-09 21:24:45 -0700

    get all the Posts

---
+++ routes/api/post-routes.js
@@ -0,0 +1,11 @@
+const routes = require('express').Router();
+const { Post, User } = require('../../models');
+
+// get all users
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+      // Query configuration
+    })
+  
+  });
